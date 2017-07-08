2d0d28cf6591a9be944337a6ee9df4e6c0eeb44c
error22 <yadavarpitbcn@gmail.com>
2017-07-09 00:57:31 +0530

    First commit

---
+++ app/src/main/java/com/example/android/recipies/BaseApplication.java
@@ -0,0 +1,19 @@
+package com.example.android.recipies;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+
+        RealmConfiguration config = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
